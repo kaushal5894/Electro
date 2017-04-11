ecf7603da300391edc70ef8a21eb8ba539815c13
kaushal5894 <ce.kbsoni94@gmail.com>
2017-04-11 22:57:58 +0530

    Add files via upload

---
+++ Electro/Electro/Party.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Party] (
+    [Party_ID]      INT           NOT NULL,
+    [Party_Name]    VARCHAR (50)  NULL,
+    [Party_Address] VARCHAR (MAX) NULL,
+    [Party_Desc] VARCHAR(MAX) NULL, 
+    PRIMARY KEY CLUSTERED ([Party_ID] ASC)
+);
+
